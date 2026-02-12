973245051f96fcfca6bec4e1dd4f3841599d587b
fullstacknyc <myemailxd@icloud.com>
2026-02-11 19:04:30 -0500

    icon

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-import { useState, useEffect } from 'react'
-
 const ResumePage = () => {
     return (
         <div className="max-w-3xl mx-auto p-4 mt-20">
             <h1 className="text-3xl font-bold mb-6">Resume</h1>
             {/* List showing polymath skills */}
-            <ul className="list-disc list-inside space-y-2 list-none">
+            <ul className="list-disc list-inside space-y-2">
                 <li>Juan Gomez</li>
                 <li>Manhattan, 10012</li>
                 <li><a href="mailto:c6m1lo@proton.me">c6m1lo@proton.me</a></li>
