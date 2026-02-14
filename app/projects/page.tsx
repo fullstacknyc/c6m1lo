f6ba314aae6f6a58adbae9492a78e6a9d5c8e733
fullstacknyc <myemailxd@icloud.com>
2026-02-14 17:51:35 -0500

    referrals

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -8,7 +8,7 @@ const ProjectsPage = () => {
       <ul className="list-disc list-inside space-y-2 text-center ">
         <li><a href="https://www.linkedin.com/in/camilogomezvalencia" target="_blank">LinkedIn</a></li>
         <li><a href="https://www.camilo777.com">Camilo777</a></li>
-        <li><a href="https://gomining.com/?ref=BM4W812" target="_blank">Cloud Mining</a></li>
+        <li><a href="/referral">Referral Page</a></li>
       </ul>
     </div>
   )
---
+++ app/referral/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const ReferralPage = () => {
+    return <div>
+        <h1 className="text-3xl font-bold mb-6 text-center m-6">Referrals</h1>
+        <ul className="list-disc list-inside space-y-2 text-center ">
+            <li><a href="https://gomining.com/?ref=BM4W812" target="_blank">Cloud Mining</a></li>
+        </ul>
+    </div>
+}
+
+export default ReferralPage;
