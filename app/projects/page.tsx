c9cc1399245279712cfc53ef7330d05ef4908fa5
fullstacknyc <myemailxd@icloud.com>
2026-02-14 17:43:39 -0500

    gomining

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -6,9 +6,9 @@ const ProjectsPage = () => {
     <div className="max-w-3xl mx-auto p-4 mt-20">
       <h1 className="text-3xl font-bold mb-6 text-center">Projects</h1>
       <ul className="list-disc list-inside space-y-2 text-center ">
+        <li><a href="https://www.linkedin.com/in/camilogomezvalencia" target="_blank">LinkedIn</a></li>
         <li><a href="https://www.camilo777.com">Camilo777</a></li>
-        <li><a href="https://www.linkedin.com/in/camilogomezvalencia">LinkedIn</a></li>
-        <li>Cross-Domain Mastery App</li>
+        <li><a href="https://gomining.com/?ref=BM4W812" target="_blank">Cloud Mining</a></li>
       </ul>
     </div>
   )
