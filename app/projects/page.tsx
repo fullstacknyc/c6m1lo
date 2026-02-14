ae6922d9277c662c7836e0fccba4761c5d85b2de
fullstacknyc <myemailxd@icloud.com>
2026-02-14 17:31:42 -0500

    recalculating course

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -7,9 +7,8 @@ const ProjectsPage = () => {
       <h1 className="text-3xl font-bold mb-6 text-center">Projects</h1>
       <ul className="list-disc list-inside space-y-2 text-center ">
         <li><a href="https://www.camilo777.com">Camilo777</a></li>
-        <li><a href="/scheduler">Scheduler App (WIP)</a></li>
-        <li><a href="https://soundcloud.com/cyamilo/sets/milo-wrld" target="_blank">Milo Wrld SoundCloud</a></li>
-        <li>Cross-Domain Mastery App (TBA)</li>
+        <li><a href="https://www.linkedin.com/in/camilogomezvalencia">LinkedIn</a></li>
+        <li>Cross-Domain Mastery App</li>
       </ul>
     </div>
   )
