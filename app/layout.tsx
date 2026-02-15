925c7310363686ba13593f8e1f9a36938b389c4b
fullstacknyc <myemailxd@icloud.com>
2026-02-15 15:29:42 -0500

    Fix AdSense import error

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Plus_Jakarta_Sans, Lora } from "next/font/google";
 import "./globals.css";
 
 import Navbar from "@/components/Navbar";
-import AdSense from "@/components/adsense";
+import AdSense from "@/components/AdSense";
 
 const bodyFont = Plus_Jakarta_Sans({
   variable: "--font-body",
